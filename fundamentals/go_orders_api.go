e5a6783e6c835f2c47f2d90fad01903eee0d5109
addetz <adelina.simion@deliveroo.co.uk>
2019-09-08 22:41:19 +0100

    Add REST api slides

---
+++ fundamentals/go_orders_api.go
@@ -0,0 +1,10 @@
+// Welcome/Index page
+    HTTP GET /
+// Find all orders
+    HTTP GET /orders
+// Find order with given id
+    HTTP GET /orders/{id}
+// Upsert order
+    HTTP POST /orders
+// Delete order with given id
+    HTTP DELETE /orders/{id}
