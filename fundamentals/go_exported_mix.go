840ecca5c0ecb71b9eb750696d0bfaa28a59dc77
addetz <adelina.simion@deliveroo.co.uk>
2019-10-16 19:33:57 +0100

    Some last few editing changes

--- fundamentals/go_exported_mix.go
+++ fundamentals/go_exported_mix.go
@@ -1,11 +1,11 @@
 package main
 
 //START OMIT
-type unexported struct { // only visible inside own package
-    Exported string // only visible inside own package
+type unexported struct {// only visible inside own package
+    Exported string     // only visible inside own package
 }
-type Exported struct { // visible outside own package
-    Exported string // visible outside own package
-    unexported string // only visible inside own package
+type Exported struct {  // visible outside own package
+    Exported string     // visible outside own package
+    unexported string   // only visible inside own package
 }
 //END OMIT
